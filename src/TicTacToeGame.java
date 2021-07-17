b1c832deb97688ba5b158b899398ed0f11eb5ff5
Mihail Nikitin <nitinin.miha@icloud.com>
2021-07-17 15:32:24 +0300

    Implemented getFieldSize() - input dialogue.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TicTacToeGame {
+import java.util.Scanner;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        int n = getFieldSize();
+    }
+    static int getFieldSize()
+    {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.println("Enter field size(odd):");
+            n = scanner.nextInt();
+        }
+        while(n%2==0);
+        scanner.close();
+        return n;
+    }
 }
